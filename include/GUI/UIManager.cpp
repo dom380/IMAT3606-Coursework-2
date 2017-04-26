155daceefb3b11399ff1896a904045a55428b3a6
JackSandham <jack.sandham@live.com>
2017-04-26 12:28:41 +0100

    UI: Fixing issue with multiple UI elements open
    Fixed an issue where having multiple UIELements would overwrite
    eachother.
    TODO: Button create + save.

--- include/GUI/UIManager.cpp
+++ include/GUI/UIManager.cpp
@@ -27,24 +27,32 @@ void UIManager::debugMenuItemUpdate()
 		vector<shared_ptr<UIElement>> uiElements = Engine::g_pEngine->getActiveScreen()->getUIElements();
 		for (int x = 0; x < uiElements.size(); x++)
 		{
+			reset();
+			if (opened.size() < uiElements.size())
+			{
+				opened.push_back(true);
+			}
 			char uiName[50];
 			snprintf(uiName, sizeof(uiName), "UI_%s_%d", uiElements[x]->getID().c_str(), x);
 			//Tree node creates a tree from the ui object name
 			if (ImGui::TreeNode(uiName))
 			{
+				hasButton = uiElements[x]->getButton() ? uiElements[x]->getButton()->isActive() : false;
+				if (!opened[x])
+				{
+					//init
+					opened[x] = true;
+				}
+				
 				//ID
-				static char uiID[64] = "";
-
 				if (strlen(uiID) == 0)
 				{
 					strncpy_s(uiID, uiElements.at(x)->getID().c_str(), uiElements.at(x)->getID().size());
 				}
 
 				ImGui::InputText("UIID", uiID, sizeof(uiID));
-				if (ImGui::Button("ApplyID"))
-				{
-					uiElements[x]->setID(uiID);
-				}
+				uiElements[x]->setID(uiID);
+				
 
 				//transform
 				DebugMenu::getInstance()->gameObjectsMenuTransform(x, uiElements[x]->getTransform().get());
@@ -52,7 +60,6 @@ void UIManager::debugMenuItemUpdate()
 				uiElements[x]->updateModelUsingTransform();
 
 				//HasButton?
-				static bool hasButton = uiElements[x]->getButton() ? uiElements[x]->getButton()->isActive() : false;
 				if (ImGui::Checkbox("HasButton", &hasButton))
 				{
 					
@@ -67,6 +74,7 @@ void UIManager::debugMenuItemUpdate()
 						{
 							//Add a button
 							LevelLoader::loadButtonElement(uiElements[x]);
+							uiElements[x]->getButton()->setActive(hasButton);
 						}
 						
 					}
@@ -74,6 +82,7 @@ void UIManager::debugMenuItemUpdate()
 					{
 						if (uiElements[x]->getButton())
 						{
+							uiElements[x]->getButton()->setActive(hasButton);
 							Engine::g_pEngine->getInput()->removeMouseListener(uiElements[x]->getButton());
 						}
 					}
@@ -82,17 +91,17 @@ void UIManager::debugMenuItemUpdate()
 				{
 					if (ImGui::TreeNode("Button"))
 					{
-						static char buttonScriptName[64] = "";
+						
 						if (strlen(buttonScriptName) == 0)
 						{
 							strncpy_s(buttonScriptName, uiElements.at(x)->getButton()->getScript().c_str(), uiElements.at(x)->getButton()->getScript().size());
 						}
-						static char buttonFuncName[64] = "";
+					
 						if (strlen(buttonFuncName) == 0)
 						{
 							strncpy_s(buttonFuncName, uiElements.at(x)->getButton()->getFunc().c_str(), uiElements.at(x)->getButton()->getFunc().size());
 						}
-						static char buttonParamID[64] = "";
+						
 						if (strlen(buttonParamID) == 0)
 						{
 							if (uiElements.at(x)->getButton()->getParams().size() > 0)
@@ -100,7 +109,7 @@ void UIManager::debugMenuItemUpdate()
 								strncpy_s(buttonParamID, uiElements.at(x)->getButton()->getParams()[0].first.c_str(), uiElements.at(x)->getButton()->getParams()[0].first.size());
 							}
 						}
-						static char buttonParamText[64] = "";
+						
 						if (strlen(buttonParamText) == 0)
 						{
 							if (uiElements.at(x)->getButton()->getParams().size() > 0)
@@ -113,14 +122,14 @@ void UIManager::debugMenuItemUpdate()
 						ImGui::InputText("FuncName", buttonFuncName, sizeof(buttonFuncName));
 						ImGui::InputText("ParamID", buttonParamID, sizeof(buttonParamID));
 						ImGui::InputText("ParamText", buttonParamText, sizeof(buttonParamText));
-
+						uiElements[x]->getButton()->setScript(buttonScriptName);
+						uiElements[x]->getButton()->setFunc(buttonFuncName);
 						if (ImGui::Button("UpdateButton"))
 						{
-							uiElements[x]->getButton()->setScript(buttonScriptName);
-							uiElements[x]->getButton()->setFunc(buttonFuncName);
 							uiElements[x]->getButton()->setParam(pair<string, string>(buttonParamID, buttonParamText));
 							LevelLoader::loadButtonFunc(uiElements[x]->getButton());
 						}
+						ImGui::SameLine();
 						if (ImGui::Button("ClearButtonParams"))
 						{
 							uiElements[x]->getButton()->clearParams();
@@ -136,19 +145,18 @@ void UIManager::debugMenuItemUpdate()
 				{
 					//edit text value
 					shared_ptr<TextBox> uiText = dynamic_pointer_cast<TextBox>(uiElements.at(x));
-					static char uiValue[64] = "";
 
-					if (strlen(uiValue) == 0)
+					if (strlen(uiTextValue) == 0)
 					{
-						strncpy_s(uiValue, uiText->getText().c_str(), uiText->getText().size());
+						strncpy_s(uiTextValue, uiText->getText().c_str(), uiText->getText().size());
 					}
 
-					ImGui::InputText("Value", uiValue, sizeof(uiValue));
-					uiText->updateText(uiValue);
+					ImGui::InputText("Value", uiTextValue, sizeof(uiTextValue));
+					uiText->updateText(uiTextValue);
 
 					//colour
 					static float dragSpeed = 0.0025f;
-					static glm::vec3 colour = uiText->getColour();
+					glm::vec3 colour = uiText->getColour();
 					ImGui::DragFloat3("Colour", &colour[0], dragSpeed);
 					uiText->setColour(colour);
 					//font
@@ -180,12 +188,29 @@ void UIManager::debugMenuItemUpdate()
 					break;
 				}
 				
-
 				ImGui::TreePop();
 			}
-			
+			else
+			{
+				opened[x] = false;
+			}
 		}
 		ImGui::EndMenu();
+	}
+}
+
+void UIManager::reset()
+{
+	hasButton = false;
+	if (strlen(uiID) != 0)
+		strncpy_s(uiID, "", 1);
+	if (strlen(buttonScriptName) != 0)
+		strncpy_s(buttonScriptName, "", 1);
+	if (strlen(buttonFuncName) != 0)
+		strncpy_s(buttonFuncName, "", 1);
+	if (strlen(uiTextValue) != 0)
+	{
+		strncpy_s(uiTextValue, "", 1);
 	}
 }
 
